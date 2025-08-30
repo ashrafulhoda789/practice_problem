78fadae2669cb449e69b0f2e0c5002d129da61c6
Ashraful Hoda Jamshed <127932275+ashrafulhoda789@users.noreply.github.com>
2025-08-30 23:51:36 +0600

    Add files via upload

---
+++ B_Grab_the_Candies.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+const int maxN = 1e5 + 9;
+vector<int> g[maxN];
+vector<bool> vis(maxN);
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+      
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        vector<int> v(n);
+        int even = 0,odd = 0;
+        for(int i=0; i<n; i++){
+            cin >> v[i];
+            if(v[i]%2 == 0) even += v[i];
+            else odd += v[i];
+        }
+
+        if(even > odd) cout << "YES\n";
+        else cout << "NO\n";
+        
+    }
+      
+    return 0;
+}
